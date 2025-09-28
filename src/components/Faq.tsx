d35b62bf938523f001dce0263efb9fd89dbe4795
Kundan2142 <e19cse078@bennett.edu.in>
2025-09-28 18:21:20 +0400

    adfsa

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -89,9 +89,10 @@ export default function FaqAccordion() {
 
   return (
     <div className="max-w-8xl mx-auto px-8 py-10">
-      <h2 className="text-4xl font-bold text-center mb-12 text-gray-900">
-        Frequently Asked Questions
-      </h2>
+      <h2 className="text-3xl sm:text-4xl font-extrabold text-center mb-12 text-gray-900">
+  Frequently Asked Questions
+</h2>
+
       <div className="space-y-8">
         {faqData.map((faq, index) => (
           <div
