01abafb8a5725434dce242eeebb5fdc7ce01a297
Kundan2142 <e19cse078@bennett.edu.in>
2025-10-02 18:52:34 +0400

    added global in json cofing

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,10 @@
-import { PrismaClient } from "@/generated/prisma"; // your generated Prisma client
-
-// Avoid multiple instances in Next.js dev mode
-// eslint-disable-next-line no-var
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+import { PrismaClient } from "@/generated/prisma"; // or "@prisma/client"
 
 // Use existing client if available, otherwise create a new one
 export const prisma =
   globalThis.prisma ??
   new PrismaClient({
-    log: ["query", "info", "warn", "error"], // optional, useful for debugging
+    log: ["query", "info", "warn", "error"], // optional
   });
 
 // Only attach to globalThis in development
